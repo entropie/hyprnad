cbba9d728aac7fefbf3a0087e6585b4a2c0fa9b2
Michael Trommer <mictro@gmail.com>
2026-08-03 14:08:29 +0200

    cln

--- modules/hyprland/host-specific.nix
+++ modules/hyprland/host-specific.nix
@@ -3,9 +3,17 @@
 let
   cfg = config.modules.hyprland;
   hostName = config.networking.hostName;
+
+
+  hyprpaperConfig =
+    if cfg.configDir == null then
+      "${inputs.self}/config/hypr/hyprpaper.${hostName}.conf"
+    else
+      "${cfg.configDir}/hyprpaper.${hostName}.conf";
+
 in
 {
-  config = lib.mkIf cfg.enable {
+  config = lib.mkIf cfg.enable {y
     home-manager.users.${cfg.user} = { config, ... }: {
       xdg.configFile."waybar/config".source =
         config.lib.file.mkOutOfStoreSymlink
@@ -14,6 +22,9 @@ in
       xdg.configFile."waybar/style.css".source =
         config.lib.file.mkOutOfStoreSymlink
           "${config.xdg.configHome}/hypr/waybar.${hostName}.css";
+
+      xdg.configFile."hyprpaper/current.conf".source =
+        config.lib.file.mkOutOfStoreSymlink hyprpaperConfig;
     };
   };
 }
