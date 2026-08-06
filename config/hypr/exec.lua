4697767ec263b4d7eafa6559fd92d029b2b1cec8
Michael Trommer <mictro@gmail.com>
2026-08-06 09:08:31 +0200

    clean

--- config/hypr/exec.lua
+++ config/hypr/exec.lua
@@ -8,23 +8,11 @@ hl.on("hyprland.start", function()
     hl.exec_cmd(uwsm_session .. "swaync")
 
     hl.exec_cmd(uwsm_session .. "nm-applet")
-    -- hl.exec_cmd(uwsm_session .. "hypridle")
-    -- hl.exec_cmd(uwsm_session .. "sway-audio-idle-inhibit")
-
-    -- hl.exec_cmd(uwsm_session .. "elephant")
-    -- hl.exec_cmd(uwsm_session .. "walker --gapplication-service")
-
 
     hl.exec_cmd(uwsm_session .. "pypr")
 
-    -- hl.exec_cmd(uwsm_background .. "hyprpaper")
-    -- hl.exec_cmd(uwsm_session .. "hypridle")
-
-
     hl.exec_cmd(uwsm_background .. "copyq")
-
     hl.exec_cmd(uwsm_background .. "jellyfin-mpv-shim")
 
     hl.exec_cmd([[mountpoint -q "$HOME/Drive" || uwsm-app -- $HOME/bin/drivemount]])
-
 end)
