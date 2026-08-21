d76755aceea7018038137558336459aa24cc5940
Michael Trommer <mictro@gmail.com>
2026-08-21 15:52:17 +0200

    tuigreet

--- modules/hyprland/default.nix
+++ modules/hyprland/default.nix
@@ -80,15 +80,27 @@ in
 
       security.pam.services.greetd.enableGnomeKeyring = true;
 
+      # services.greetd = {
+      #   enable = true;
+
+      #   settings.default_session = {
+      #     command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --remember-session";
+      #     user = "greeter";
+      #   };
+      # };
+
       services.greetd = {
         enable = true;
+        useTextGreeter = true;
 
         settings.default_session = {
-          command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --remember-session";
+          command = ''
+            ${pkgs.tuigreet}/bin/tuigreet --time --remember --remember-session  --sessions ${config.services.displayManager.sessionData.desktops}/share/wayland-sessions
+          '';
           user = "greeter";
         };
       };
-
+      
       # Display Manager
       services.displayManager = {
         gdm.enable = false;
