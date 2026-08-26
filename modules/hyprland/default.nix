f6d210f3d7e31748eafae3f6a051419a4ddb317a
Michael Trommer <mictro@gmail.com>
2026-08-26 04:24:05 +0200

    cachix

--- modules/hyprland/default.nix
+++ modules/hyprland/default.nix
@@ -89,6 +89,13 @@ in
       #   };
       # };
 
+      nix.settings = {
+        substituters = [ "https://hyprland.cachix.org" ];
+        trusted-substituters = [ "https://hyprland.cachix.org" ];
+        trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
+        trusted-users = [ "root" "@wheel" ];
+      };
+      
       services.greetd = {
         enable = true;
         useTextGreeter = true;
